dc01bb7f1a4e3a248ea7bf97526a3e765dfcb913
hcymt3 <117790901+hcymt3@users.noreply.github.com>
2023-03-23 13:51:07 +0800

    Update SettingsView.swift
    need to check if DONE button on keypad works.
    
    Fixed colours to match main screen colour sheme, kept to white background to make clear distinction between main page and settings page.
    
    Attempted to fix "settings" logo and title not appearing in simulator

--- WeatherApp/SettingsView.swift
+++ WeatherApp/SettingsView.swift
@@ -21,35 +21,30 @@ struct SwiftUIView: View {
         
         ZStack {
             
-            Color.white
-                .edgesIgnoringSafeArea(.all)
+            Color.white.edgesIgnoringSafeArea(.all)
                         
             VStack{
                 
                 //heading
                 HStack{
                     
-                    Spacer()
-                    
                     Text("Settings")
                         .font(.system(size: 40))
+                        .foregroundColor(.black)
+                        
                                                         
                     Image(systemName: "gearshape.fill")
                         .resizable()
                         .frame(width: 30, height: 30)
-                    
-                    Spacer()
+                        .foregroundColor(.black)
 
-                    
                 }
                 
+                
                 Text("Tap to adjust upper and lower limits")
-        
-                //to separate contents
-                Spacer()
-                    .frame(height: 80)
-                
-                
+                    .foregroundColor(.black)
+
+                        
                 //UPPER BOUND SETTINGS
                 
                 if !editUB{ //if no edits are being made
@@ -78,7 +73,7 @@ struct SwiftUIView: View {
                         .padding(EdgeInsets(top: 50, leading: 30, bottom: 50, trailing: 30))
                         
                     }
-                    .background(Color.red.opacity(0.8))
+                    .background(Gradient(colors: [Color.cyan, Color.blue]))
                     .cornerRadius(10)
                     .padding(50)
                     
@@ -91,39 +86,34 @@ struct SwiftUIView: View {
                         
                         HStack{
                             
-                            TextField(" Enter upperBound", text: $UB)
+                            TextField(" Enter upper bound", text: $UB)
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .padding(.horizontal, 60)
-                                .keyboardType(.numberPad)
-                            
+                                .keyboardType(.decimalPad)
+
                         }
-                        .padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+                        .padding(EdgeInsets(top: 70, leading: 0, bottom: 70, trailing: 0))
                         
-                        Button("Done") {
-                            
-                            if ( Double(UB) ?? data.upperBound > data.lowerBound)
-                            {
-                                data.upperBound = Double(UB) ?? data.upperBound
-                            }
-                            
-                            editUB = false
-                            
-                        }
-                        .padding(30)
+//                        Button("Done") {
+//
+//                            if ( Double(UB) ?? data.upperBound > data.lowerBound)
+//                            {
+//                                data.upperBound = Double(UB) ?? data.upperBound
+//                            }
+//
+//                            editUB = false
+//
+//                        }
+//                        .padding(30)
                                     
                 }
-                .background(Color.gray.opacity(0.5))
+                .background(Color.blue.opacity(0.5))
                 .cornerRadius(10)
                 .padding(50)
     
                 }
-            
-                                
-
-//                Spacer()
-//                    .frame(height: 80)
-//
                 
+
                 //LOWER BOUND SETTINGS
                 
                 if !editLB{ //if no edits are being made
@@ -152,11 +142,13 @@ struct SwiftUIView: View {
                         .padding(EdgeInsets(top: 50, leading: 30, bottom: 50, trailing: 30))
                         
                     }
-                    .background(Color.red.opacity(0.8))
+                    .background(Gradient(colors: [Color.cyan, Color.blue]))
                     .cornerRadius(10)
                     .padding(50)
                     
                 }
+                
+                
                 
                 //edit screen
                 if editLB {
@@ -165,33 +157,45 @@ struct SwiftUIView: View {
                         
                         HStack{
                             
-                            TextField(" Enter lowerBound", text: $UB)
+                            TextField(" Enter lower bound", text: $LB)
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .padding(.horizontal, 60)
-                                .keyboardType(.numberPad)
-                            
-                        }
-                        .padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+                                .keyboardType(.decimalPad)
+                                .toolbar {
+                                    ToolbarItem(placement: .keyboard) {
+                                        Spacer()
+                                        Button("Done") {
+                                            if ( Double(LB) ?? data.lowerBound < data.upperBound)
+                                            {
+                                                data.lowerBound = Double(LB) ?? data.lowerBound
+                                            }
+                                            
+                                            editLB = false
+                                        }
+                                    }
+                                }
+                                }
+                                .padding(EdgeInsets(top: 70, leading: 0, bottom: 70, trailing: 0))
                         
-                        Button("Done") {
+                        //REMOVED DONE BUTTON
+//                        Button("Done") {
+//
+//                            if ( Double(LB) ?? data.lowerBound < data.upperBound)
+//                            {
+//                                data.lowerBound = Double(LB) ?? data.lowerBound
+//                            }
+//
+//                            editLB = false
+//
+//                        }
+//                        .padding(30)
                             
-                            if ( Double(LB) ?? data.lowerBound < data.upperBound)
-                            {
-                                data.lowerBound = Double(LB) ?? data.lowerBound
-                            }
-                            
-                            editLB = false
-                            
-                        }
-                        .padding(30)
-                                    
                 }
-                .background(Color.gray.opacity(0.5))
+                .background(Color.blue.opacity(0.5))
                 .cornerRadius(10)
                 .padding(50)
     
                 }
-
                 
             }
         }
